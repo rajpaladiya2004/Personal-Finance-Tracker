0ea35cb3d7500b2b08605462a7b2405ded1180ab
rajpaladiya2026 <rajpaladiya2023@gmail.com>
2026-08-07 01:49:55 +0200

    Make user field required on Category, Transaction, and Notification

--- expenses/models.py
+++ expenses/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     def __str__(self):
@@ -27,7 +27,7 @@ class Transaction(models.Model):
         ('expense', 'Expense'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
@@ -44,7 +44,7 @@ class Transaction(models.Model):
 
 
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     message = models.CharField(max_length=255)
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='notifications', null=True, blank=True)
